43e0c77bc64557d9305d89bfa246ec46b8e2dfc8
Aditi.Ch06 <aditi.ch06@gmail.com>
2024-12-10 23:59:12 +0530

    again modified

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,17 +1,17 @@
-@app.route('/', methods=["GET"])
-def home():
-    return jsonify({"message": "Flask app is running!"})
+# @app.route('/', methods=["GET"])
+# def home():
+#     return jsonify({"message": "Flask app is running!"})
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    print("request received")
-    input_data = request.get_json()
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     print("request received")
+#     input_data = request.get_json()
 
-    if not input_data:
-        return jsonify("error: No data received")
+#     if not input_data:
+#         return jsonify("error: No data received")
     
-    if "text" not in input_data:
-        return jsonify("error: 'text' field is required")
+#     if "text" not in input_data:
+#         return jsonify("error: 'text' field is required")
 
-    output = predict_sentiment(input_data['text'])
-    return jsonify(f"Predicted Sentiment: {output}")
+#     output = predict_sentiment(input_data['text'])
+#     return jsonify(f"Predicted Sentiment: {output}")
